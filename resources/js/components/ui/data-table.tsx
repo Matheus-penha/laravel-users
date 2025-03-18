94a5cfe6a213650d485cf0aa4974e9a5fac6961e
Matheus Penha <matheus.penha28@gmail.com>
2025-03-18 16:22:23 -0300

    Commit final

--- resources/js/components/ui/data-table.tsx
+++ resources/js/components/ui/data-table.tsx
@@ -4,7 +4,7 @@ import {
   getCoreRowModel,
   getFilteredRowModel,
   getPaginationRowModel,
-  useReactTable, // Adicionar isso
+  useReactTable, 
 } from "@tanstack/react-table";
 
 import {
@@ -14,46 +14,48 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table";
+} from "@/components/ui/table"; 
 import Pagination from "./pagination";
-import { Input } from "./input";
-
+import { Input } from "./input"; 
 import { Key, ComponentType, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
 
-
-
 interface DataTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[];
-  data: TData[];
+  columns: ColumnDef<TData, TValue>[]; // Definição das colunas da tabela
+  data: TData[]; // Dados da tabela
 }
 
+// Função que renderiza a tabela
 export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  // hook useReactTable
   const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-    getFilteredRowModel: getFilteredRowModel(), // Necessário para o filtro
-    getPaginationRowModel: getPaginationRowModel(), // Adicionando isso
+    data, // Dados que serão exibidos na tabela
+    columns, // Colunas da tabela
+    getCoreRowModel: getCoreRowModel(), 
+    getFilteredRowModel: getFilteredRowModel(), 
+    getPaginationRowModel: getPaginationRowModel(),
   });
 
   return (
     <div className="rounded-md border">
+      
       <div className="flex items-center py-4">
         <Input
           placeholder="Filter names..."
-          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""} // filtro da coluna "name"
           className="max-w-sm"
           onChange={(event) =>
-            table.getColumn("name")?.setFilterValue(event.target.value)
+            table.getColumn("name")?.setFilterValue(event.target.value) // Atualiza o filtro quando o valor do input muda
           }
         />
       </div>
 
+      {/* Tabela */}
       <Table>
         <TableHeader>
+          {/* Iterando sobre os grupos de cabeçalhos da tabela */}
           {table.getHeaderGroups().map((headerGroup: { id: Key | null | undefined; headers: any[]; }) => (
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header: { id: Key | null | undefined; isPlaceholder: any; column: { columnDef: { header: string | number | bigint | boolean | ComponentType<any> | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; }; }; getContext: () => any; }) => {
@@ -71,18 +73,26 @@ export function DataTable<TData, TValue>({
             </TableRow>
           ))}
         </TableHeader>
+
+        {/* corpo da Tabela */}
         <TableBody>
+          {/* Verificando se há linhas para renderizar */}
           {table.getRowModel().rows?.length ? (
             table.getRowModel().rows.map((row: { id: Key | null | undefined; getIsSelected: () => any; getVisibleCells: () => any[]; }) => (
-              <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+              <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}> {/* Verifica se a linha está selecionada */}
+                {/* Renderizando células visíveis da linha */}
                 {row.getVisibleCells().map((cell: { id: Key | null | undefined; column: { columnDef: { cell: string | number | bigint | boolean | ComponentType<any> | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; }; }; getContext: () => any; }) => (
                   <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    {flexRender(
+                      cell.column.columnDef.cell,
+                      cell.getContext() // Passa o contexto para o renderizador da célula
+                    )}
                   </TableCell>
                 ))}
               </TableRow>
             ))
           ) : (
+            // Se não houver resultados
             <TableRow>
               <TableCell colSpan={columns.length} className="h-24 text-center">
                 No results.
@@ -91,6 +101,8 @@ export function DataTable<TData, TValue>({
           )}
         </TableBody>
       </Table>
+
+      {/* Componente de Paginação */}
       <Pagination table={table} />
     </div>
   );
--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -8,8 +8,8 @@ export function cn(...inputs: ClassValue[]) {
 export const getAvatar = (name: string) => {
     const names = name.split(" ");
     const firstNameInitial = names[0]?.substring(0, 1); // Primeira letra do primeiro nome
-    const secondNameInitial = names[1]?.substring(0, 1).toUpperCase(); // Primeira letra do segundo nome, se existir
+    const secondNameInitial = names[1]?.substring(0, 1).toUpperCase(); // Primeira letra do segundo nome, caso exista
 
-    // Se não houver segundo nome, apenas a inicial do primeiro nome é usada
+    // Se não tiver segundo nome, apenas a inicial do primeiro nome é usada
     return firstNameInitial + (secondNameInitial || "");
 }
